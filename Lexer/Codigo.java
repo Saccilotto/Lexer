8c6f9f3d92966d6fbc84379890b2e2ddbf8e472f
Saccilotto <andre.santos01@edu.pucrs.br>
2022-03-20 00:03:15 -0300

    new Codigo.java version

--- Lexer/Codigo.java
+++ Lexer/Codigo.java
@@ -9,30 +9,30 @@ public class Codigo{
 
     void menu(){
         Scanner scanner = new Scanner(System.in);
-        boolean iteration = true;
-        while (iteration) {
-            System.out.println("Digite o nome do arquivo de texto (presente no diretório atual): ");
-            String file = scanner.next();
-            String fullPath = new File("").getAbsolutePath() + "/" + file;
-            if(fullPath.equals("") || fullPath.equals(null)) {
-                System.out.println("Diretorio invalido.");
-            } else {
-                try {
-                    BufferedReader reader = new BufferedReader(new FileReader(fullPath));
-                    String currentLine = reader.readLine();
-                    Analisador analisador = new Analisador(currentLine);
-                    ArrayList<Lexema> lex = analisador.getLexema();
-    
-                    for(int i = 0;i < analisador.getLexema().size();i++){
-                        System.out.println("('"+ lex.get(i).getValor() + ", " + lex.get(i).getToken() + ", " + lex.get(i).getToken().idToken + ")");
-                    }
-                    reader.close();
-                } catch(IOException ex){
-                    System.out.println("Insira um diretório válido.");
+        //boolean iteration = false;
+        // do {
+        System.out.println("Digite o nome do arquivo de texto (presente no diretorio atual): ");
+        String file = scanner.next();
+        String fullPath = new File("").getAbsolutePath() + "/" + file;
+        if(fullPath.equals("") || fullPath.equals(null)) {
+            System.out.println("Diretorio invalido.");
+            // iteration = true;
+        } else {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(fullPath));
+                String currentLine = reader.readLine();
+                Analisador analisador = new Analisador(currentLine);
+                ArrayList<Lexema> lex = analisador.getLexema();
+
+                for(int i = 0;i < analisador.getLexema().size();i++){
+                    System.out.println("('"+ lex.get(i).getValor() + ", " + lex.get(i).getToken() + ", " + lex.get(i).getToken().idToken + ")");
                 }
-                iteration = false;
+            } catch(IOException ex){
+                System.out.println("Insira um diretorio valido.");
+                //iteration = true;
             }
-            scanner.close();
-        }
+        }     
+        scanner.close();
+        // } while(iteration);
     }
 }
